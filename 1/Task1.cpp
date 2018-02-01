434cf6ae2a895fac510be2034d8c3d177d099142
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-01 20:19:06 +0300

    Update Task1.cpp

--- 1/Task1.cpp
+++ 1/Task1.cpp
@@ -5,23 +5,38 @@
 */
 
 #include <iostream>
-#define SIZE 10
+#include <ctime>
+#define SIZE 111
 void randArr(int *a, int sizeArr);
 void printArr(int *a, int sizeArr);
 using namespace std;
 
 int main()
-{	
+{
 	setlocale(LC_ALL, "rus");
 	int myArr[SIZE];
+	srand(time(0));
 	randArr(myArr, SIZE);
 	printArr(myArr, SIZE);
-	cout << endl;
+	int *ptrMy = myArr;
+	int countOfNegative = 0;
+	int countOfPositive = 0;
+	while (ptrMy < (myArr + SIZE))
+	{
+		if (*ptrMy > 0) countOfPositive++;
+		if (*ptrMy < 0) countOfNegative++;
+		ptrMy++;
+	}
+	double percentPos, percentNeg;
+	percentNeg = double(countOfNegative * 100) / SIZE;
+	cout << countOfNegative;
+	percentPos = double(countOfPositive * 100) / SIZE;
+	printf("\nПоложительных элементов %3.2f %%, Отрицательных элементов %3.2f %%\n", percentPos, percentNeg);
 	system("pause");
 	return 0;
 }
 
-void randArr(int *a, int sizeArr) {	
+void randArr(int *a, int sizeArr) {
 	for (int i = 0; i<sizeArr; i++, a++)
 		*a = rand() % 91 - 45;
 }
