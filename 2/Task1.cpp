900564affd87c19720e7157693d1f99eb77c904e
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-01 20:53:01 +0300

    Update Task1.cpp

--- 2/Task1.cpp
+++ 2/Task1.cpp
@@ -1,12 +1,17 @@
-/*4. Даны два массива: А[n] и B[m]. Необходимо создать третий массив, в котором
-нужно собрать:
+/*
+1. Даны два массива: А[n] и B[m] (сгенерировать случайным образом).
+Необходимо создать еще два массива, в которые нужно собрать:
 • Общие элементы двух массивов;
-• Элементы массива A, которые не включаются в B;*/
+• Элементы массива A, которые не включаются в B;
+Повторов в новых массивах не должно быть! (т.е. если общий элемент повторяется
+в исходном массиве несколько раз, то в результирующем массиве он присутствует
+только один раз)
+*/
 
 #include <iostream>
 #include <ctime>
-#define NSIZE 33
-#define MSIZE 12
+#define NSIZE 7
+#define MSIZE 6
 using namespace std;
 void randArr(int a[], int sizeArr);
 void printArr(int a[], int sizeArr);
@@ -18,62 +23,56 @@ void main()
 	srand(time(0));
 	int arrA[NSIZE];
 	int arrB[MSIZE];
-	int arrC[NSIZE] = {};
-	//Генерация сортированного массива  А[n]:
+	int arrC[MSIZE];
+	int arrD[NSIZE];
+	//Генерация  массива  А[n]:
 	//(Инициализируем случайными числами)
 	{
 		randArr(arrA, NSIZE);
-		/*cout << "Исходный массив А[n]:" << endl;
-		printArr(arrA, NSIZE);*/
-		sortArr(arrA, NSIZE);
-		cout << "Сортированный массив А[n]:" << endl;
+		cout << "Исходный массив А[n]:" << endl;
 		printArr(arrA, NSIZE);
+		
 	}
-	//Генерация сортированного массива  B[m]:
+	//Генерация  массива  B[m]:
 	{
 		randArr(arrB, MSIZE);
-		/*cout << "Исходный массив B[m]:" << endl;
-		printArr(arrB, MSIZE);*/
-		sortArr(arrB, MSIZE);
-		cout << "Сортированный массив B[m]:" << endl;
+		cout << "Исходный массив B[m]:" << endl;
 		printArr(arrB, MSIZE);
+		
 	}
 
-	int *ptrA, *ptrB, *ptrC;
+	int *ptrA, *ptrB, *ptrC, *ptrD;
+	ptrA = arrA;
+	ptrB = arrB;
 	ptrC = arrC;
-	int col = 0;
-	for (ptrA = arrA; ptrA < &arrA[NSIZE]; ptrA++) {
-		for (ptrB = arrB; ptrB<&arrB[MSIZE]; ptrB++)
+	int colGeneral = 0;
+	while (ptrA < arrA + NSIZE)
+	{
+		bool flag = true;
+		int *tmpptr = arrC;
+		while (tmpptr < arrC + MSIZE)
 		{
-			if (*ptrA == *ptrB&& *ptrA != *(ptrA - 1)) {
-				*ptrC = *ptrA;
-				ptrC++;
-				col++;
+			if (*tmpptr == *ptrA) {
+				flag = false;
 				break;
 			}
+			tmpptr++;
 		}
-	}
-	
-	//Вставляем разделитель
-	{
-		*ptrC = 111;
-		ptrC++;
-		col++;
-	}
-	for (ptrA = arrA; ptrA < &arrA[NSIZE]; ptrA++) {
-		int flag = 0;
-		for (ptrB = arrB; ptrB<&arrB[MSIZE]; ptrB++)
+		while (flag && ptrB < arrB + MSIZE)
 		{
-			if (*ptrA == *ptrB) flag++;
-		}
-		if (flag < 1 && *ptrA != *(ptrA + 1)) {
-			*ptrC = *ptrA;
-			ptrC++;
-			col++;
+			if (*ptrA == *ptrB) {
+				*ptrC++ = *ptrA;
+				colGeneral++;
+				break;
+			}
+			ptrB++;
 		}
 	}
+
+	
+
 	cout << endl << "Итоговый массив C[]" << endl;
-	printArr(arrC, col);
+	printArr(arrC, colGeneral);
 	system("pause");
 }
 
