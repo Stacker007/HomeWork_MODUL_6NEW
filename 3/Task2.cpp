652394e81ae4583494b0901fcbe9ffc9df2eae45
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-11 11:46:39 +0300

    Update Task2.cpp

--- 3/Task2.cpp
+++ 3/Task2.cpp
@@ -1,51 +1,55 @@
 /*
-1. Создать массив целых чисел размерностью N и заполнить его случайными
-числами от -20 до 20. Объявить два массива указателей. Заполнить первый из них
-указателями на положительные числа исходного массива, а второй – указателями на
-отрицательные числа. Пользуясь массивами указателей, вывести на консоль сначала
-положительные, а потом отрицательные значения.
+2. Двумерный массив размерностью N строк на M столбцов заполнить
+случайными целыми числами от -10 до 10. 
+Сформировать массив указателей наначало каждой строки двумерного массива. 
+Отсортировать этот массив указателей
+так, чтобы первые элементы строк оказались в порядке возрастания (сам двумерный
+массив не изменяется, меняется только массив указателей). Вывести строки
+двумерного массива в полученном порядке.
 */
 
 #include <iostream>
 #include <ctime>
-#define COL 20
+#define ROW 10
+#define COL 2
 using namespace std;
 void randArr(int *a, int sizeArr);
+void multiRandArr(int *a, int row, int col);
 void printArr(int *a, int sizeArr);
-void printArr(int **a, int sizeArr);
+void multiPrintArr(int *a, int row, int col);
+
 void main()
 {
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
-	int myArr[COL];
-	int *posPtr[COL], *negPtr[COL];
-	randArr(myArr, COL);
-	printArr(myArr, COL);
-	int *myPtr = myArr;
-	int **positivePtr = posPtr;
-	int **negativePtr = negPtr;
-	int countOfPos = 0, countOfNeg = 0;
+	int myArr[ROW][COL] = {};
+	multiRandArr(myArr[0], ROW, COL);
+	multiPrintArr(myArr[0], ROW, COL);
 	cout << endl << endl;
-	while (myPtr < myArr + COL)
+
+	int *a[ROW];
+	int *rowPtr = myArr[0];
+
+	int i = 0;
+	while (rowPtr < myArr[0] + ROW * COL)
 	{
-		if (*myPtr < 0)
-		{
-			*negativePtr++ = myPtr;
-			countOfNeg++;
-		}
-		if (*myPtr > 0) 
-		{
-			*positivePtr++ = myPtr;
-			countOfPos++;
-		}
-		myPtr++;
+		a[i++] = rowPtr;
+		rowPtr += COL;
 	}
 
-	printArr(negPtr, countOfNeg);
-	cout << endl << endl;
-	printArr(posPtr, countOfPos);
-	cout << endl << endl;
-	
+	for (int k = ROW - 1; k > 0; k--) //Сортировка методом пузырька
+		for (int i = 0; i < k; i++)
+			if (*a[i] > *a[i + 1])//Здесь определяется порядок вывода
+			{
+				int *tmp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = tmp;
+			}
+	for (int i = 0; i<ROW; i++)
+	{
+		printArr(a[i], COL);
+		cout << endl;
+	}
 	system("pause");
 }
 
@@ -53,14 +57,20 @@ void main()
 
 void randArr(int *a, int sizeArr) {
 	for (int i = 0; i<sizeArr; i++, a++)
-		*a = rand() % 41 - 20;
+		*a = rand() % 21 - 10;
 }
-
+void multiRandArr(int *a, int row, int col) {
+	for (int i = 0; i < row; i++, a += col) {
+		randArr(a, col);
+	}
+}
 void printArr(int *a, int sizeArr) {
 	for (int i = 0; i < sizeArr; i++, a++)
 		printf("%3d ", *a);
 }
-void printArr(int **a, int sizeArr) {
-	for (int i = 0; i < sizeArr; i++, a++)
-		printf("%3d ", **a);
+void multiPrintArr(int *a, int row, int col) {
+	for (int i = 0; i < row; i++, a += col) {
+		printArr(a, col);
+		cout << endl;
+	}
 }
